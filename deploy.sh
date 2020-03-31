8b2f369c4ec8f50380d2a58dda033dd00ffd96fe
Reshma Khilnani <reshma.khilnani@gmail.com>
2020-03-31 15:14:24 -0700

    Adding volunteer opportunities and timestamp

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 aws s3 cp index.html s3://identify-immune-www/ --acl public-read
 aws s3 cp team.html s3://identify-immune-www/ --acl public-read
 aws s3 cp error.html s3://identify-immune-www/ --acl public-read
+aws s3 cp volunteer.html s3://identify-immune-www/ --acl public-read
 aws s3 cp success.html s3://identify-immune-www/ --acl public-read
 aws s3 cp identify-immune.js s3://identify-immune-www/ --acl public-read
 aws s3 cp data-states.csv s3://identify-immune-www/ --acl public-read
@@ -20,6 +21,7 @@ aws s3 cp team.png s3://identify-immune-www/ --acl public-read
 aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/index.html"
 aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/success.html"
 aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/error.html"
+aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/volunteer.html"
 aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/team.html"
 aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/identify-immune.js"
 aws cloudfront create-invalidation --distribution-id E382EUS5SP9ZAI --paths "/team.html"
